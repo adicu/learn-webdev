d92f43f4b6241ace7d27359fb1474c4dacca5113
Nate Brennand <natebrennand@gmail.com>
2014-01-09 21:36:49 -0800

    convert app.py to spaces

--- app.py
+++ app.py
@@ -6,43 +6,43 @@ app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+    return "Hello World!"
 
 @app.route("/name")
 def name():
-	return "ADI"
+    return "ADI"
 
 @app.route("/website")
 def website():
-	return "http://adicu.com"
+    return "http://adicu.com"
 
 @app.route("/search/<search_query>")
 def search(search_query):
-	url = "https://api.github.com/search/repositories?q=" + search_query
-	response_dict = requests.get(url).json()
-	if "items" not in response_dict:
-		return jsonify(response_dict)
-	else:
-		return jsonify(parse_response(response_dict))
+    url = "https://api.github.com/search/repositories?q=" + search_query
+    response_dict = requests.get(url).json()
+    if "items" not in response_dict:
+        return jsonify(response_dict)
+    else:
+        return jsonify(parse_response(response_dict))
 
 def parse_response(response_dict):
-	clean_dict = {
-		"total_count": response_dict["total_count"],
-		"items":[]
-	}
-	for repo in response_dict["items"]:
-		clean_repo = {
-			"name": repo["name"],
-			"owner": {
-				"login": repo["owner"]["login"],
-				"avatar_url": repo["owner"]["avatar_url"],
-				"html_url": repo["owner"]["html_url"]
-			},
-			"html_url": repo["html_url"],
-			"description": repo["description"]
-		}
-		clean_dict["items"].append(clean_repo)
-	return clean_dict
+    clean_dict = {
+        "total_count": response_dict["total_count"],
+        "items":[]
+    }
+    for repo in response_dict["items"]:
+        clean_repo = {
+            "name": repo["name"],
+            "owner": {
+                "login": repo["owner"]["login"],
+                "avatar_url": repo["owner"]["avatar_url"],
+                "html_url": repo["owner"]["html_url"]
+            },
+            "html_url": repo["html_url"],
+            "description": repo["description"]
+        }
+        clean_dict["items"].append(clean_repo)
+    return clean_dict
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
